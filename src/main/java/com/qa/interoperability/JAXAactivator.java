3a5ed57b58679c914ba938bb5856e1f485359b11
Marcell92 <illesmarcell92@gmail.com>
2018-04-29 23:33:59 +0200

    jaxaActivator and endpoint created appropriately

---
+++ src/main/java/com/qa/interoperability/JAXAactivator.java
@@ -0,0 +1,9 @@
+package com.qa.interoperability;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class JAXAactivator extends Application {
+
+}
